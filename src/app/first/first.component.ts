d9bad29c2965d8a5437c366cce45001f4458619f
Jorge Mena <jmena9496@gmail.com>
2025-04-30 08:17:05 -0600

    Improved original code, deleted unnecessary code

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,107 +1,86 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css',
-  
 })
+export class FirstComponent {
 
-export class FirstComponent implements OnInit {
-
+myForm = new FormGroup({
+  variable: new FormControl(''),
+  variable2: new FormControl('', {
+    validators: [],
+  }),
+});
 
   localeTasks: string[] = [];
+  completedTasks: string[] = [];
+  pendingTasks: string[] = [];
 
   MarkedAsCompleted = false;
 
- newValue= '';
-searchValue= '' ;
-foundValue=''
- 
- status =""
+  newValue = '';
+  searchValue = '';
+  foundValue = '';
 
- completedTasks: string[]= [];
- pendingTasks: string[] = []
+  submitted = '';
 
-addCompleted(){
-  this.completedTasks.push(this.newValue)
-}
- 
-addPending(){
-  // this.completedTasks.push(this.newValue)
-}
-
-onSubmit(){
-
-  console.log('Submitted!')
-window.localStorage.setItem(this.newValue, this.newValue);
-this.localeTasks.push(this.newValue)
-this.status='Successfully Submitted!'
-setTimeout(()=>{this.status = ''},3000) 
-
-}
-
-onFind(){
-
-let x=0;
-for(x; x<this.localeTasks.length; x++){
-  if(this.localeTasks[x] == this.searchValue){
-    this.foundValue = this.localeTasks[x];
-    return 
-  }else{
-    this.foundValue = 'Task not found'
-  }
-};
-
-
-}
-
-showDropDown = false;
-menu = ''
-showDropdownMenu(){
-  this.showDropDown = true
-}
-
-
-boolean = false
-
-
-x=0;
-holder: string= ''
-
-showAll = ()=>{
- console.log('SHOW ALL')
- 
-this.boolean = true
-
-};
-
-
-
-showPending(){
- 
-  console.log(window.localStorage)
-
-};
-
-showCompleteTasks = false
-showComplete(){
- 
-  this.showCompleteTasks = true
- 
-};
-
-ngOnInit(): void {
   
+
+  addPending() {
+    // this.completedTasks.push(this.newValue)
+  }
+
+  onSubmit() {
+    
+    console.log('Submitted!');
+    console.log(this.myForm)
+    window.localStorage.setItem(this.newValue, this.newValue);
+    this.localeTasks.push(this.newValue);
+    this.submitted = 'Successfully Submitted!';
+    setTimeout(() => {
+      this.submitted = '';
+    }, 3000);
+  }
+
+  onFind() {
+    let x = 0;
+    for (x; x < this.localeTasks.length; x++) {
+      if (this.localeTasks[x] == this.searchValue) {
+        this.foundValue = this.localeTasks[x];
+        return;
+      } else {
+        this.foundValue = 'Task not found';
+      }
+    }
+  }
+
+
+   allStorage: string[] = [];
+
+  showAll = () => {
+    console.log('SHOW ALL');
+    
+    let x = 0;
+    for(x; x<window.localStorage.length; x++){
+this.allStorage.push(window.localStorage.key(x)!)}
+    console.log(this.allStorage)
+  };
+
+  showPending() {
+    console.log(window.localStorage);
+  }
+
+  showCompleteTasks = false;
+  showComplete() {
+    this.showCompleteTasks = true;
+  }
+
+deleteTask(){
+  console.log('Delete Task')
+  window.localStorage.removeItem('')
 }
-
-
-
-
-
-
 }
